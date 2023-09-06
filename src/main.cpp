a8fe1f3017d3ec4a6e13c7a116a934ddfc4cc760
jymy <jymyko@gmail.com>
2023-09-06 12:25:38 +0300

    WIP change to bitboards

--- src/main.cpp
+++ src/main.cpp
@@ -4,23 +4,33 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <cstdint>
+#include <bitset>
+
+typedef uint64_t bitboard;
 
 enum Piece
 {
-    PAWN_WHITE = 9,
-    KNIGHT_WHITE = 10,
-    BISHOP_WHITE = 11,
-    ROOK_WHITE = 12,
-    QUEEN_WHITE = 13,
-    KING_WHITE = 14,
-    PAWN_BLACK = 1,
-    KNIGHT_BLACK = 2,
-    BISHOP_BLACK = 3,
-    ROOK_BLACK = 4,
-    QUEEN_BLACK = 5,
-    KING_BLACK = 6,
-    EMPTY = 0,
-    INVALID = -1
+    PAWN_WHITE = 0,
+    KNIGHT_WHITE = 1,
+    BISHOP_WHITE = 2,
+    ROOK_WHITE = 3,
+    QUEEN_WHITE = 4,
+    KING_WHITE = 5,
+    PAWN_BLACK = 8,
+    KNIGHT_BLACK = 9,
+    BISHOP_BLACK = 10,
+    ROOK_BLACK = 11,
+    QUEEN_BLACK = 12,
+    KING_BLACK = 13
+};
+
+enum CastlingMasks
+{
+    WHITE_KINGSIDE = 0b1000,
+    WHITE_QUEENSIDE = 0b0100,
+    BLACK_KINGSIDE = 0b0010,
+    BLACK_QUEENSIDE = 0b0001
 };
 
 std::vector<std::string> splitString(std::string string, char splitter)
@@ -47,16 +57,13 @@ std::vector<std::string> splitString(std::string string, char splitter)
 
 int squareToIndex(std::string square)
 {
-    return (square[1] - 47) * 10 + (square[0] - 95);
+    return (square[1] - 48) * 8 + (square[0] - 65);
 }
 
 std::string numberToPiece(int piece)
 {
     switch (piece & 0b1111)
     {
-        case EMPTY:
-            return " ";
-            break;
         case PAWN_WHITE:
             return "♟";
             break;
@@ -98,14 +105,6 @@ std::string numberToPiece(int piece)
     }
 }
 
-struct CastlingRights
-{
-    bool blackKingSide = true;
-    bool blackQueenSide = true;
-    bool whiteKingSide = true;
-    bool whiteQueenSide = true;
-};
-
 struct Move
 {
     int startSquare;
@@ -115,191 +114,132 @@ struct Move
 class Game
 {
   public:
-    int board[120] = {
-        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-        -1, 0, 0, 0, 0, 0, 0, 0, 0, -1,
-        -1, 0, 0, 0, 0, 0, 0, 0, 0, -1,
-        -1, 0, 0, 0, 0, 0, 0, 0, 0, -1,
-        -1, 0, 0, 0, 0, 0, 0, 0, 0, -1,
-        -1, 0, 0, 0, 0, 0, 0, 0, 0, -1,
-        -1, 0, 0, 0, 0, 0, 0, 0, 0, -1,
-        -1, 0, 0, 0, 0, 0, 0, 0, 0, -1,
-        -1, 0, 0, 0, 0, 0, 0, 0, 0, -1,
-        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1};
-
     int currentTurn = 1; // 1 for white, -1 for black
-    CastlingRights castlingAvailability;
-    int enpassant = 0;
+    uint8_t castlingRights;
+    uint64_t enpassant = 0;
     int halfMoves = 0;
 
-    Game(std::string fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
+    bitboard bitboards[16];
+
+    void setFromFen(std::string fen) 
+    { 
+        uint64_t currentSquare = 1;
+        std::vector<std::string> fenParts = splitString(fen, ' ');
+        for(char &letter : fenParts[0])
+        {
+            if (isdigit(letter))
+            {
+                currentSquare = currentSquare << ((letter) - 48);
+            }
+            else 
+            {
+                switch(letter)
+                {
+                    case 'P':
+                        bitboards[PAWN_WHITE] = bitboards[PAWN_WHITE] | currentSquare;
+                        currentSquare = currentSquare << 1;
+                        break;
+                    case 'p':
+                        bitboards[PAWN_BLACK] = bitboards[PAWN_BLACK] | currentSquare;
+                        currentSquare = currentSquare << 1;
+                        break;
+                    case 'R':
+                        bitboards[ROOK_WHITE] = bitboards[ROOK_WHITE] | currentSquare;
+                        currentSquare = currentSquare << 1;
+                        break;
+                    case 'r':
+                        bitboards[ROOK_BLACK] = bitboards[ROOK_BLACK] | currentSquare;
+                        currentSquare = currentSquare << 1;
+                        break;
+                    case 'N':
+                        bitboards[KNIGHT_WHITE] = bitboards[KNIGHT_WHITE] | currentSquare;
+                        currentSquare = currentSquare << 1;
+                        break;
+                    case 'n':
+                        bitboards[KNIGHT_BLACK] = bitboards[KNIGHT_BLACK] | currentSquare;
+                        currentSquare = currentSquare << 1;
+                        break;
+                    case 'B':
+                        bitboards[BISHOP_WHITE] = bitboards[BISHOP_WHITE] | currentSquare;
+                        currentSquare = currentSquare << 1;
+                        break;
+                    case 'b':
+                        bitboards[BISHOP_BLACK] = bitboards[BISHOP_BLACK] | currentSquare;
+                        currentSquare = currentSquare << 1;
+                        break;
+                    case 'Q':
+                        bitboards[QUEEN_WHITE] = bitboards[QUEEN_WHITE] | currentSquare;
+                        currentSquare = currentSquare << 1;
+                        break;
+                    case 'q':
+                        bitboards[QUEEN_BLACK] = bitboards[QUEEN_BLACK] | currentSquare;
+                        currentSquare = currentSquare << 1;
+                        break;
+                    case 'K':
+                        bitboards[KING_WHITE] = bitboards[KING_WHITE] | currentSquare;
+                        currentSquare = currentSquare << 1;
+                        break;
+                    case 'k':
+                        bitboards[KING_BLACK] = bitboards[KING_BLACK] | currentSquare;
+                        currentSquare = currentSquare << 1;
+                        break;
+                    case '/':
+                        break;
+                    default: 
+                        std::cerr << "invalid fen (Includes invalid pieces)" << std::endl; 
+                }
+            }
+
+            if(fenParts[1] == "w") 
+            {
+                currentTurn = 1;
+            } 
+            else 
+            {
+                currentTurn = -1;
+            }
+
+            for(char &letter : fenParts[2]) 
+            {
+                switch(letter) 
+                {
+                    case 'K':
+                        castlingRights = castlingRights | WHITE_KINGSIDE;
+                        break;
+                    case 'Q':
+                        castlingRights = castlingRights | WHITE_QUEENSIDE;
+                        break;
+                    case 'k':
+                        castlingRights = castlingRights | BLACK_KINGSIDE;
+                        break;
+                    case 'q':
+                        castlingRights = castlingRights | BLACK_QUEENSIDE;
+                        break;
+                }
+            }
+
+            if (fenParts[3] == "-")
+            {
+                continue;
+            }
+            else 
+            {
+                enpassant = squareToIndex(fenParts[3]);
+            }
+        }
+    }
+
+    Game(std::string fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq a1 0 1") 
     {
         setFromFen(fen);
     }
-
-    void setFromFen(std::string fen)
-    {
-        std::vector<std::string> fenParts = splitString(fen, ' ');
-
-        if (fenParts.size() != 6 || fenParts[1].size() != 1)
-        {
-            std::cerr << "Hurr durr, huono fen (fen väärän pituinen)" << std::endl;
-            return;
-        }
-
-        for (int i = 2; i < 10; i++) // clear board
-        {
-            for (int j = 1; j < 9; j++)
-            {
-                board[i * 10 + j] = EMPTY;
-            }
-        }
-
-        int currentSquare = 21;
-        for (char fenPart : fenParts[0])
-        {
-            switch (fenPart)
-            {
-                case 'P':
-                    board[currentSquare] = PAWN_WHITE;
-                    currentSquare++;
-                    break;
-                case 'p':
-                    board[currentSquare] = PAWN_BLACK;
-                    currentSquare++;
-                    break;
-                case 'N':
-                    board[currentSquare] = KNIGHT_WHITE;
-                    currentSquare++;
-                    break;
-                case 'n':
-                    board[currentSquare] = KNIGHT_BLACK;
-                    currentSquare++;
-                    break;
-                case 'B':
-                    board[currentSquare] = BISHOP_WHITE;
-                    currentSquare++;
-                    break;
-                case 'b':
-                    board[currentSquare] = BISHOP_BLACK;
-                    currentSquare++;
-                    break;
-                case 'R':
-                    board[currentSquare] = ROOK_WHITE;
-                    currentSquare++;
-                    break;
-                case 'r':
-                    board[currentSquare] = ROOK_BLACK;
-                    currentSquare++;
-                    break;
-                case 'Q':
-                    board[currentSquare] = QUEEN_WHITE;
-                    currentSquare++;
-                    break;
-                case 'q':
-                    board[currentSquare] = QUEEN_BLACK;
-                    currentSquare++;
-                    break;
-                case 'K':
-                    board[currentSquare] = KING_WHITE;
-                    currentSquare++;
-                    break;
-                case 'k':
-                    board[currentSquare] = KING_BLACK;
-                    currentSquare++;
-                    break;
-                case '/':
-                    currentSquare += 2;
-                    break;
-                case '1':
-                    currentSquare += 1;
-                    break;
-                case '2':
-                    currentSquare += 2;
-                    break;
-                case '3':
-                    currentSquare += 3;
-                    break;
-                case '4':
-                    currentSquare += 4;
-                    break;
-                case '5':
-                    currentSquare += 5;
-                    break;
-                case '6':
-                    currentSquare += 6;
-                    break;
-                case '7':
-                    currentSquare += 7;
-                    break;
-                case '8':
-                    currentSquare += 8;
-                    break;
-            }
-        }
-
-        switch (fenParts[1][0])
-        {
-            case 'w':
-                currentTurn = 1;
-                break;
-            case 'b':
-                currentTurn = -1;
-                break;
-            default:
-                std::cerr << "Hurr durr, huono fen (Vuoroindikaattori)" << std::endl;
-                break;
-        }
-
-        for (char castlingDirection : fenParts[2])
-        {
-            switch (castlingDirection)
-            {
-                case 'K':
-                    castlingAvailability.whiteKingSide = true;
-                    break;
-                case 'Q':
-                    castlingAvailability.whiteKingSide = true;
-                    break;
-                case 'k':
-                    castlingAvailability.blackKingSide = true;
-                    break;
-                case 'q':
-                    castlingAvailability.blackQueenSide = true;
-                    break;
-                case '-':
-                    break;
-                default:
-                    std::cerr << "Hurr durr, huono fen (linnoittautumis suunnat)" << std::endl;
-                    break;
-            }
-        }
-
-        if (fenParts[3] != "-")
-            enpassant = squareToIndex(fenParts[3]);
-        else
-            enpassant = -1;
-    }
-
+    
     void print()
     {
-        std::string row = "+-----+-----+-----+-----+-----+-----+-----+-----+";
-        for (int i = 2; i < 10; i++)
-        {
-            std::cout << row << '\n';
-            std::cout << "|";
-
-            for (int j = 1; j < 9; j++)
-            {
-                std::cout << "  " << numberToPiece(board[i * 10 + j]) << "  ";
-                std::cout << "|";
-            }
-            std::cout << std::endl;
-        }
-        std::cout << row << std::endl;
-    }
+        std::bitset<64> x(bitboards[PAWN_WHITE]);
+        std::cout << x << std::endl;
+        std::cout << enpassant << std::endl;
+    }   
 };
 
 int main()
